c864753c3a5e989e1cdfd58718d288a1dc0ee9ed
99Kies <1290017556@qq.com>
2023-06-18 17:34:36 +0800

    Init Qrng Contract

--- contracts/QrngUserDemo.sol
+++ contracts/QrngUserDemo.sol
@@ -4,27 +4,17 @@ import "./QrngCoordinator.sol";
 import "./ConsumerBase.sol";
 
 contract QrngUserDemo is ConsumerBase {
-    uint32 callbackGasLimit = 21000000;
+    uint32 callbackGasLimit = 21_000_000;
     uint32 numWords = 1;
     address owner;
 
     QrngCoordinator COORDINATOR;
-
+		
     address doraCoordinatorAddr = 0x9676fC93257876428e2D0284B5e5C12a718F1D1E;
     
-
     uint256 public requestId;
     uint256[] public s_randomWords;
     
-    struct data {
-        uint256 request_id;
-        uint256[] randomes;
-    }
-
-    mapping (uint256 => data) public result;
-    
-    uint32 count = 0;
-
     constructor() ConsumerBase(doraCoordinatorAddr)  {
         COORDINATOR = QrngCoordinator(doraCoordinatorAddr);
         owner = msg.sender;
@@ -42,7 +32,5 @@ contract QrngUserDemo is ConsumerBase {
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
         s_randomWords = randomWords;
-        result[count] = data(requestId, randomWords);
-        count += 1;
     }
 }
